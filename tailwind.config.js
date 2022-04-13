b407532c07cb97404ef1bc5d88ac33eae17e5550
Victor Prim <victorprimbuchele@gmail.com>
2022-04-13 07:29:41 -0300

    refactor: reconfigurada opções de estilização removido sass alterado para css e add dir ao tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{tsx, ts, js, jsx}", "index.html"],
+  content: [
+    "./src/presentation/components/**/*.{tsx, ts, js, jsx}",
+    "./src/presentation/pages/**/*.{tsx, ts, js, jsx}",
+    "./index.html",
+  ],
   theme: {
     extend: {},
   },
