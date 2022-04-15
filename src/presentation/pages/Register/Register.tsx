798929b700c32b9f9b5907a6f7039c01e6af65cf
Victor Prim <victorprimbuchele@gmail.com>
2022-04-15 11:19:18 -0300

    adicionado estilo e evento de clique ao chevron do componente de registro

--- src/presentation/pages/Register/Register.tsx
+++ src/presentation/pages/Register/Register.tsx
@@ -1,16 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+
 import { Form } from "../../components/Form/Form";
 import { NeumorphicBox } from "../../components/Neumorphic/Box/Box";
 import RegisterFormData from "../../../data/form/register/register-form-data.json";
 
 export const Register: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="grid place-content-center justify-center content-center h-full">
       <NeumorphicBox size="md" className="rounded-lg p-6">
         <div className="grid grid-cols-3 text-center content-center py-3 mb-5 items-center text-xl font-semibold">
-          <FontAwesomeIcon icon={faChevronLeft} />
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            className=" p-3 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
           <h1>Register</h1>
         </div>
         <div>
