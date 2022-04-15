f18d651bc8967567c43e54457a557c63f126db68
Victor Prim <victorprimbuchele@gmail.com>
2022-04-15 00:54:59 -0300

    feat: aplicação de componente neumórfico

--- src/presentation/pages/Register/Register.tsx
+++ src/presentation/pages/Register/Register.tsx
@@ -8,7 +8,7 @@ import RegisterFormData from "../../../data/form/register/register-form-data.jso
 export const Register: React.FC = () => {
   return (
     <div className="grid place-content-center justify-center content-center h-full">
-      <NeumorphicBox size="md" className="rounded-lg p-4">
+      <NeumorphicBox size="md" className="rounded-lg p-6">
         <div className="grid grid-cols-3 text-center content-center py-3">
           <FontAwesomeIcon icon={faChevronLeft} />
           <h1>Register</h1>
