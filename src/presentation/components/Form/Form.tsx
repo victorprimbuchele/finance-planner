35f9997c16ad9746d59231500abd036ff13749e2
Victor Prim <victorprimbuchele@gmail.com>
2022-04-15 11:21:41 -0300

    refactor: passada tipagem do form para seu próprio arquivo

--- src/presentation/components/Form/Form.tsx
+++ src/presentation/components/Form/Form.tsx
@@ -1,28 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-
-interface FormParams {
-  input: InputProps[];
-  children?: React.ReactNode;
-}
-
-type InputProps = {
-  placeholder?: string;
-  required: boolean;
-  type: string;
-  name: string;
-  label: string;
-  errors?: string;
-  display?: "flex" | "grid";
-  gridCols?: string | number | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-
-  gridGap?: string | number | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-  grouped: boolean;
-  group?: InputProps[];
-  min?: number;
-  max?: number;
-  class?: any;
-};
+import { FormParams } from "./form-types";
 
 export const Form: React.FC<FormParams> = ({ input, children }) => {
   const {
