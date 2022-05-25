ae440767c995a73ece8694d21b45d046adeccc1c
Victor Prim <victorprimbuchele@gmail.com>
2022-05-25 07:36:40 -0300

    feature/assegurar que o usuário continue conectado ao recarregar a página

--- src/infrastructure/integrations/finance-planner-api/index.ts
+++ src/infrastructure/integrations/finance-planner-api/index.ts
@@ -3,15 +3,28 @@ import axios from "axios";
 const timeout = import.meta.env.VITE_FINANCE_PLANNER_API_TIMEOUT;
 const baseURL = import.meta.env.VITE_FINANCE_PLANNER_API_BASE_URL;
 
+const token = localStorage.getItem("token");
+
 export const apiFinancePlanner = axios.create({
   baseURL: baseURL,
   timeout: timeout,
   headers: {
     "Content-Type": "application/json",
-
-    authorization:
-      localStorage.getItem("token") != null
-        ? (localStorage.getItem("token") as string)
-        : "",
+    authorization: token ? token : "",
   },
 });
+
+// Adicionar um interceptador de resposta
+apiFinancePlanner.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Caso o usuário esteja desautorizado ou desautenticado, seu token é removido e ele é redirecionado para a página de login
+    if (error.response.status === 401 || error.response.status === 403) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
